4925375c6d1643a2eb07168be7f7a5920ef54593
Redhouane Sobaihi <s-redouane@outlook.com>
2019-11-20 20:37:15 +0700

    Initial commit - splash screen.

---
+++ app/src/main/java/com/github/redouane64/debt/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.github.redouane64.debt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
